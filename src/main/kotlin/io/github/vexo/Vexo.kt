f8bbc5571249650f431866628b8dcf669c3f0775
lloyd838 <pk.bearlest@gmail.com>
2025-08-07 16:30:49 +0200

    Add ModID Hider

--- src/main/kotlin/io/github/vexo/Vexo.kt
+++ src/main/kotlin/io/github/vexo/Vexo.kt
@@ -3,13 +3,14 @@ package io.github.vexo
 import io.github.vexo.features.PrintTest
 import io.github.vexo.features.chat.ChatCleaner
 import io.github.vexo.features.dungeons.HideMageSheep
+import net.minecraft.client.Minecraft
 import net.minecraftforge.client.ClientCommandHandler
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 
 
-@Mod(modid = "vexo", useMetadata = true)
+@Mod(modid = Vexo.MOD_ID, useMetadata = true)
 class Vexo {
     @Mod.EventHandler
     fun init(event: FMLInitializationEvent) {
@@ -19,5 +20,12 @@ class Vexo {
 
         ClientCommandHandler.instance.registerCommand(PrintTest())
         MinecraftForge.EVENT_BUS.register(this)
+    }
+
+    companion object {
+        const val MOD_ID = "vexo"
+
+        @JvmStatic
+        val mc : Minecraft = Minecraft.getMinecraft()
     }
 }
