31d7492acf2e071d86a8203f4d2a448b2bab59d9
Traube <ben.trautner@web.de>
2025-08-10 11:57:59 +0200

    Improved Feature Importing

--- src/main/kotlin/io/github/vexo/Vexo.kt
+++ src/main/kotlin/io/github/vexo/Vexo.kt
@@ -7,25 +7,34 @@ import io.github.vexo.features.dungeons.RagAxeNow
 import io.github.vexo.features.dungeons.Tyfr
 import io.github.vexo.features.dungeons.EndOfRun
 import net.minecraft.client.Minecraft
+import net.minecraft.command.ICommand
 import net.minecraftforge.client.ClientCommandHandler
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 
+fun register(handler: Any) {
+    MinecraftForge.EVENT_BUS.register(handler)
+}
+
+fun cmd(command: ICommand) {
+    ClientCommandHandler.instance.registerCommand(command)
+}
 
 @Mod(modid = Vexo.MOD_ID, useMetadata = true)
 class Vexo {
     @Mod.EventHandler
     fun init(event: FMLInitializationEvent) {
-        MinecraftForge.EVENT_BUS.register(MyEventHandlerClass())
-        MinecraftForge.EVENT_BUS.register(HideMageSheep())
-        MinecraftForge.EVENT_BUS.register(ChatCleaner())
-        MinecraftForge.EVENT_BUS.register(RagAxeNow())
-        MinecraftForge.EVENT_BUS.register(EndOfRun())
+        register(MyEventHandlerClass())
+        register(HideMageSheep())
+        register(ChatCleaner())
+        register(RagAxeNow())
+        register(EndOfRun())
 
-        ClientCommandHandler.instance.registerCommand(PrintTest())
-        ClientCommandHandler.instance.registerCommand(Tyfr())
-        MinecraftForge.EVENT_BUS.register(this)
+        cmd(PrintTest())
+        cmd(Tyfr())
+
+        register(this)
     }
 
     companion object {
--- src/main/kotlin/io/github/vexo/features/dungeons/EndOfRun.kt
+++
@@ -1,8 +0,0 @@
-package io.github.vexo.features.dungeons
-
-import io.github.vexo.utils.skyblock.sendCommand
-import net.minecraftforge.client.event.ClientChatReceivedEvent
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-
-
-
