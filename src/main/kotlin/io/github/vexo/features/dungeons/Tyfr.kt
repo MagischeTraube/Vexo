37c8d085e82d53dba72b65d68fc64818075ebe93
lloyd838 <pk.bearlest@gmail.com>
2025-08-11 10:32:39 +0200

    Reverted tyfr

--- src/main/kotlin/io/github/vexo/features/dungeons/Tyfr.kt
+++ src/main/kotlin/io/github/vexo/features/dungeons/Tyfr.kt
@@ -8,7 +8,9 @@ import net.minecraft.command.CommandException
 import net.minecraft.command.ICommandSender
 import net.minecraftforge.client.event.ClientChatReceivedEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent
 import java.util.*
+
 object tyfrData {
     var tyfr = false
     val TyfrTrigger = listOf(
@@ -17,10 +19,7 @@ object tyfrData {
     var msgDelay = 0
 }
 
-
 class Tyfr : CommandBase() {
-
-
 
     override fun getCommandName(): String {
         return "tyfr"
@@ -32,9 +31,13 @@ class Tyfr : CommandBase() {
 
     @Throws(CommandException::class)
     override fun processCommand(sender: ICommandSender?, args: Array<String?>?) {
-        tyfrData.tyfr = true
-        tyfrData.msgDelay = 5
-        modMessage("TYFR activated! – waiting for the end of the run")}
+        tyfrData.tyfr = !tyfrData.tyfr
+        if (tyfrData.tyfr) {
+            modMessage("TYFR activated! – waiting for the end of the run")
+        } else {
+            modMessage("TYFR deactivated!")
+        }
+    }
 
     override fun canCommandSenderUseCommand(sender: ICommandSender?): Boolean {
         return true
@@ -49,11 +52,16 @@ class EndOfRun {
     @SubscribeEvent
     fun onChat(event: ClientChatReceivedEvent) {
         if (tyfrData.TyfrTrigger.any { it.containsMatchIn(event.message.getFormattedText()) } && tyfrData.tyfr) {
+            tyfrData.msgDelay = 5
             sendCommand("p leave")
-            if (tyfrData.msgDelay > 0) tyfrData.msgDelay--
-            else {
-                sendCommand("ac tyfr o/")
-            }
+        }
+    }
+
+    @SubscribeEvent
+    fun onServerTick(event: TickEvent.ServerTickEvent) {
+        if (tyfrData.msgDelay > 0) tyfrData.msgDelay--
+        else {
+            sendCommand("ac tyfr o/")
         }
     }
 }
