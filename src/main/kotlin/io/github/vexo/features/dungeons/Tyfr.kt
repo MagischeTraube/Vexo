b01b72b220f50205c1411071a16af5a06f984540
Traube_V2 <ben.trautner@web.de>
2025-08-14 10:04:36 +0200

    Changed Tyfr to trigger in Dungeons and Kuudra

--- src/main/kotlin/io/github/vexo/features/dungeons/Tyfr.kt
+++ src/main/kotlin/io/github/vexo/features/dungeons/Tyfr.kt
@@ -1,13 +1,13 @@
 package io.github.vexo.features.dungeons
 
 
+import io.github.vexo.events.ChatPacketEvent
 import io.github.vexo.events.ServerTickEvent
 import io.github.vexo.utils.skyblock.modMessage
 import io.github.vexo.utils.skyblock.sendCommand
 import net.minecraft.command.CommandBase
 import net.minecraft.command.CommandException
 import net.minecraft.command.ICommandSender
-import net.minecraftforge.client.event.ClientChatReceivedEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.util.*
 
@@ -52,8 +52,9 @@ object Tyfr : CommandBase() {
 
 object EndOfRun {
     @SubscribeEvent
-    fun onChat(event: ClientChatReceivedEvent) {
-        if (event.message.getFormattedText().contains("Score") && tyfrData.tyfr) {
+    fun onChat(event: ChatPacketEvent) {
+        // (event.message.getFormattedText() && tyfrData.tyfr)
+        if (tyfrData.tyfr && tyfrData.TyfrTrigger.any { it.containsMatchIn(event.message) }){
             tyfrData.EndOfRun = true
             sendCommand("p leave")
             tyfrData.msgDelay = 5
