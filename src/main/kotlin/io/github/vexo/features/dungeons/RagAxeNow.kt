8bc4b52e07d21aabb18bce0e6e38866cf4e94e2b
Traube_V2 <ben.trautner@web.de>
2025-08-29 13:01:50 +0200

    Changed Regex in RagAxeNow

--- src/main/kotlin/io/github/vexo/features/dungeons/RagAxeNow.kt
+++ src/main/kotlin/io/github/vexo/features/dungeons/RagAxeNow.kt
@@ -3,7 +3,6 @@ package io.github.vexo.features.dungeons
 import io.github.vexo.config.*
 import io.github.vexo.events.ChatPacketEvent
 import io.github.vexo.utils.skyblock.modMessage
-import io.github.vexo.utils.*
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 
@@ -19,8 +18,8 @@ object RagAxeNow : Module(
         }
     }
     private val RagAxeTriggers = listOf(
-        Regex("[BOSS] Wither King: I no longer wish to fight, but I know that will not stop you."),
-        Regex("[BOSS] Livid: I can now turn those Spirits into shadows of myself, identical to their creator."),
-        Regex("[BOSS] Sadan: I am the bridge between this realm and the world below! You shall not pass!")
+        Regex("\\[BOSS] Wither King: I no longer wish to fight, but I know that will not stop you."),
+        Regex("\\[BOSS] Livid: I can now turn those Spirits into shadows of myself, identical to their creator."),
+        Regex("\\[BOSS] Sadan: I am the bridge between this realm and the world below! You shall not pass!")
     )
 }
