e28164afc185e89f82c75679ae0ea3d62ff56719
Traube <ben.trautner@web.de>
2025-08-31 09:59:39 +0200

    Fixed Compiler failing

--- src/main/kotlin/io/github/vexo/Vexo.kt
+++ src/main/kotlin/io/github/vexo/Vexo.kt
@@ -24,7 +24,7 @@ class Vexo {
     fun init(event: FMLInitializationEvent) {
 
         listOf(
-            EventTrigger, EndOfRun, checkForUpdateOnStartup, PriceUtils, IGNUtils
+            EventTrigger, EndOfRun, checkForUpdateOnStartup, PriceUtils
         ).forEach { MinecraftForge.EVENT_BUS.register(it) }
 
 
