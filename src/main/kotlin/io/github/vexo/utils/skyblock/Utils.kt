67e6cf9fc9509215e1e20b85272f17639fe3cae0
Traube <ben.trautner@web.de>
2025-09-05 13:08:30 +0200

    Removed Tickutils

--- src/main/kotlin/io/github/vexo/utils/skyblock/Utils.kt
+++ src/main/kotlin/io/github/vexo/utils/skyblock/Utils.kt
@@ -3,22 +3,3 @@ package io.github.vexo.utils.skyblock
 fun String.removeFormatting() : String {
     return this.replace(Regex("§."), "")
 }
-
-object TickDelayUtils {
-
-    private val delayedActions = mutableMapOf<Int, MutableList<() -> Unit>>()
-    private var currentTick = 0
-
-    fun tickDelay(ticks: Int, action: () -> Unit) {
-        val targetTick = currentTick + ticks
-        delayedActions.getOrPut(targetTick) { mutableListOf() }.add(action)
-    }
-
-    fun onTick() {
-        currentTick++
-        delayedActions[currentTick]?.let { actions ->
-            actions.forEach { it() }
-            delayedActions.remove(currentTick)
-        }
-    }
-}
