3fbc1fefe7d4ee334814e69139464ca0c3512c2a
lloyd838 <pk.bearlest@gmail.com>
2025-09-28 10:20:44 +0200

    Add Commission Highlight

---
+++ src/main/kotlin/io/github/vexo/features/QOL/CommissionHighlight.kt
@@ -0,0 +1,11 @@
+package io.github.vexo.features.QOL
+
+import io.github.vexo.config.Module
+
+object CommissionHighlight : Module(
+    name = "Example",
+    description = "Ein Beispielmodul, das verschiedene Einstellungen demonstriert.",
+    category = "Test"
+) {
+
+}
