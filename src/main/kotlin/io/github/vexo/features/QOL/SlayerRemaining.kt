8e1d19916d6e24b07a7ad4170247539b6208c39c
Traube <ben.trautner@web.de>
2025-12-25 11:18:35 +0100

    finally fixed slayer boss tracker

--- src/main/kotlin/io/github/vexo/features/QOL/SlayerRemaining.kt
+++ src/main/kotlin/io/github/vexo/features/QOL/SlayerRemaining.kt
@@ -1,5 +1,6 @@
 package io.github.vexo.features.QOL
 
+import kotlin.math.ceil
 import io.github.vexo.config.*
 import io.github.vexo.events.ChatPacketEvent
 import io.github.vexo.utils.skyblock.*
@@ -8,23 +9,47 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 
 object SlayerRemaining : Module(
-    name = "Slayer Bosses Remaiing",
+    name = "Slayer Bosses Remaining",
     description = "Tells you how many Slayer bosses you need to kill to reach the new level",
     category = "QOL"
 ) {
-    private val XP_PerKill = registerSetting(InputSetting("Slayer EXP per Kill:", "500", "Einstellen"))
-    private val xp_given = registerSetting(InputSetting("Your current Remaining xp:", "1000000", "Einstellen"))
+    private var currentXp: Int = 0
+    private var previousXp: Int = 0
+    private var xpPerKill: Int = 0
 
-    private var current_xp = xp_given.value.toInt()
-    private var boesses_remaining =
-        current_xp / XP_PerKill.value.toInt()
-
+    val SLAYER_XP_REGEX = """- Next LVL in ([\d,]+) XP!""".toRegex()
 
     @SubscribeEvent(receiveCanceled = true)
     fun onChat(event: ChatPacketEvent) {
-        if (event.message.removeFormatting() == "  SLAYER QUEST COMPLETE!") {
-            current_xp = current_xp - XP_PerKill.value.toInt()
-            modMessage("You need " + boesses_remaining + " more bosses to the next level")
+        val message = event.message.removeFormatting()
+
+        // XP aus der Slayer-Info extrahieren
+        var matchedRegex = updateSlayerStatistics(message)
+
+        // Bei Quest-Complete die XP pro Kill berechnen und verbleibende Bosse ausgeben
+        if (matchedRegex && xpPerKill > 0) {
+
+                val bossesRemaining = ceil(currentXp.toDouble() / xpPerKill.toDouble()).toInt()
+
+                modMessage("You need $bossesRemaining more bosses to reach the next level (${xpPerKill} XP per kill)")
+
+            }
+        }
+
+
+
+        fun updateSlayerStatistics(message: String): Boolean {
+            val matchResult = SLAYER_XP_REGEX.find(message)
+
+            if (matchResult != null) {
+                val xpString = matchResult.groupValues[1]
+                previousXp = currentXp
+                currentXp = xpString.replace(",", "").toInt()
+                xpPerKill = previousXp - currentXp
+
+                return true
+            }
+
+            return false
         }
     }
-}
